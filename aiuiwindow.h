fe17f5a7786d47b8ccba9a7f4826ae46cb0dabe2
wang2 <wang2@qq.com>
2024-06-21 10:03:58 +0800

    add aiui

---
+++ aiuiwindow.h
@@ -0,0 +1,16 @@
+#ifndef AIUIWINDOW_H
+#define AIUIWINDOW_H
+#include "AIUI_v2.h"
+#include <QCoreApplication>
+#include <QProcess>
+#include <QDebug>
+
+QString getWindowsSerialNumber();
+
+class aiuiwindow
+{
+public:
+    aiuiwindow();
+};
+
+#endif // AIUIWINDOW_H
